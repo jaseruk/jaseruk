a58b5b20b8532456f0c9bea5beaa1d7e94656fcf
jaseruk <102226183+jaseruk@users.noreply.github.com>
2022-11-23 08:49:56 +0000

    Add CA Template for block

--- AssignPermissionsLogicApp.ps1
+++ AssignPermissionsLogicApp.ps1
@@ -5,6 +5,7 @@ $PermissionDirReadAll = $graph.AppRoles | where Value -Like "Directory.Read.All"
 $PermissionPolicyReadAll = $graph.AppRoles | where Value -Like "Policy.Read.All" | Select-Object -First 1
 $PermissionPolicyRWCA = $graph.AppRoles | where Value -Like "Policy.ReadWrite.ConditionalAccess" | Select-Object -First 1
 $PermissionPolicyAppReadAll = $graph.AppRoles | where Value -Like "Application.Read.All" | Select-Object -First 1
+$PermissionPolicyReadAllAuditLog = $graph.AppRoles | where Value -Like "Auditlog.Read.All" | Select-Object -First 1
 
 
 $msi = Get-AzureADServicePrincipal -ObjectId 92856a23-7172-48da-9f72-48fc5d7ee7af
@@ -13,3 +14,4 @@ New-AzureADServiceAppRoleAssignment -Id $PermissionDirReadAll.Id -ObjectId $msi.
 New-AzureADServiceAppRoleAssignment -Id $PermissionPolicyReadAll.Id -ObjectId $msi.ObjectId -PrincipalId $msi.ObjectId -ResourceId $graph.ObjectId
 New-AzureADServiceAppRoleAssignment -Id $PermissionPolicyRWCA.Id -ObjectId $msi.ObjectId -PrincipalId $msi.ObjectId -ResourceId $graph.ObjectId
 New-AzureADServiceAppRoleAssignment -Id $PermissionPolicyAppReadAll.Id -ObjectId $msi.ObjectId -PrincipalId $msi.ObjectId -ResourceId $graph.ObjectId
+New-AzureADServiceAppRoleAssignment -Id $PermissionPolicyReadAllAuditLog.Id -ObjectId $msi.ObjectId -PrincipalId $msi.ObjectId -ResourceId $graph.ObjectId
